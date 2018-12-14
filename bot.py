64141ee3f8d23bc12a2cb75d56cf1a771b6d905b
skhachko <skhachko@cogniance.com>
2018-12-14 17:15:57 +0200

    adding bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+from config import token
+from weather import Weather, Unit
+import telebot
+import helpers.helper as helper
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start', 'go', 'testRun'])
+def start_handler(message):
+    # print(helper.tb_message_to_dict(message))
+    bot.send_message(message.chat.id, "Hello, I'm a bot")
+
+
+@bot.message_handler(content_types=['location'])
+def location_handler(message):
+    weather = Weather(Unit.CELSIUS)
+    l = weather.lookup_by_latlng(message.location.latitude, message.location.longitude)
+    out = ("{}, {}, {}" + u'\N{DEGREE SIGN}').format(l.location.region, l.location.country, l.condition.temp)
+    bot.send_message(message.chat.id, out)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
